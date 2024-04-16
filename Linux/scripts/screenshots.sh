8ca30d3c6e8a9e617b973c81bbbf714b61c635ce
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2024-04-16 23:32:32 +0300

    Delete Linux directory

--- Linux/scripts/screenshots.sh
+++
@@ -1,6 +0,0 @@
-# Take screenshot of current active monitor
-
-active_workspace_monitor=$(hyprctl -j activeworkspace | jq -r '(.monitor)')
-screenshot_filename="$HOME/Pictures/Screenshots/$(date +"%d-%m-%Y-%H%S")-$active_workspace_monitor.png"
-
-grim -o $active_workspace_monitor $screenshot_filename
