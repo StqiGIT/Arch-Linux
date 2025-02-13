0262d0eba849c5aaaebfdd243fda5f21dcd050e4
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2025-02-13 22:28:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,20 @@ timedatectl set-ntp true
 virt_check () {
     hypervisor=$(systemd-detect-virt)
     case $hypervisor in
-        kvm )   info_print "KVM has been detected, setting up guest tools."
+        kvm )   echo "KVM has been detected, setting up guest tools."
                 pacstrap /mnt qemu-guest-agent &>/dev/null
                 systemctl enable qemu-guest-agent --root=/mnt &>/dev/null
                 ;;
-        vmware  )   info_print "VMWare Workstation/ESXi has been detected, setting up guest tools."
+        vmware  )   echo "VMWare Workstation/ESXi has been detected, setting up guest tools."
                     pacstrap /mnt open-vm-tools >/dev/null
                     systemctl enable vmtoolsd --root=/mnt &>/dev/null
                     systemctl enable vmware-vmblock-fuse --root=/mnt &>/dev/null
                     ;;
-        oracle )    info_print "VirtualBox has been detected, setting up guest tools."
+        oracle )    echo "VirtualBox has been detected, setting up guest tools."
                     pacstrap /mnt virtualbox-guest-utils &>/dev/null
                     systemctl enable vboxservice --root=/mnt &>/dev/null
                     ;;
-        microsoft ) info_print "Hyper-V has been detected, setting up guest tools."
+        microsoft ) echo "Hyper-V has been detected, setting up guest tools."
                     pacstrap /mnt hyperv &>/dev/null
                     systemctl enable hv_fcopy_daemon --root=/mnt &>/dev/null
                     systemctl enable hv_kvp_daemon --root=/mnt &>/dev/null
@@ -30,12 +30,12 @@ virt_check () {
 }
 
 kernel_selector () {
-    info_print "List of kernels:"
-    info_print "1) Stable: Vanilla Linux kernel with a few specific Arch Linux patches applied"
-    info_print "2) Hardened: A security-focused Linux kernel"
-    info_print "3) Longterm: Long-term support (LTS) Linux kernel"
-    info_print "4) Zen Kernel: A Linux kernel optimized for desktop usage"
-    input_print "Please select the number of the corresponding kernel (e.g. 1): "
+    echo "List of kernels:"
+    echo "1) Stable: Vanilla Linux kernel with a few specific Arch Linux patches applied"
+    echo "2) Hardened: A security-focused Linux kernel"
+    echo "3) Longterm: Long-term support (LTS) Linux kernel"
+    echo "4) Zen Kernel: A Linux kernel optimized for desktop usage"
+    echo "Please select the number of the corresponding kernel (e.g. 1): "
     read -r kernel_choice
     case $kernel_choice in
         1 ) kernel="linux"
@@ -54,20 +54,20 @@ kernel_selector () {
 microcode_detector () {
     CPU=$(grep vendor_id /proc/cpuinfo)
     if [[ "$CPU" == *"AuthenticAMD"* ]]; then
-        info_print "An AMD CPU has been detected, the AMD microcode will be installed."
+        echo "An AMD CPU has been detected, the AMD microcode will be installed."
         microcode="amd-ucode"
     else
-        info_print "An Intel CPU has been detected, the Intel microcode will be installed."
+        echo "An Intel CPU has been detected, the Intel microcode will be installed."
         microcode="intel-ucode"
     fi
 }
 
 network_selector () {
-    info_print "Network utilities:"
-    info_print "1) IWD: Utility to connect to networks written by Intel (WiFi-only, built-in DHCP client)"
-    info_print "2) NetworkManager: Universal network utility (both WiFi and Ethernet, highly recommended)"
-    info_print "3) dhcpcd: Basic DHCP client (Ethernet connections or VMs)"
-    input_print "Please select the number of the corresponding networking utility (e.g. 1): "
+    echo "Network utilities:"
+    echo "1) IWD: Utility to connect to networks written by Intel (WiFi-only, built-in DHCP client)"
+    echo "2) NetworkManager: Universal network utility (both WiFi and Ethernet, highly recommended)"
+    echo "3) dhcpcd: Basic DHCP client (Ethernet connections or VMs)"
+    echo "Please select the number of the corresponding networking utility (e.g. 1): "
     read -r network_choice
     if ! ((1 <= network_choice <= 3)); then
         error_print "You did not enter a valid selection, please try again."
@@ -77,7 +77,7 @@ network_selector () {
 }
 
 hostname_selector () {
-    input_print "Please enter the hostname: "
+    echo "Please enter the hostname: "
     read -r hostname
     if [[ -z "$hostname" ]]; then
         error_print "You need to enter a hostname in order to continue."
@@ -87,7 +87,7 @@ hostname_selector () {
 }
 
 locale_selector () {
-    input_print "Please insert the locale you use (format: xx_XX. Enter empty to use en_US, or \"/\" to search locales): " locale
+    echo "Please insert the locale you use (format: xx_XX. Enter empty to use en_US, or \"/\" to search locales): " locale
     read -r locale
     case "$locale" in
         '/') sed -E '/^# +|^#$/d;s/^#| *$//g;s/ .*/ (Charset:&)/' /etc/locale.gen | less -M
@@ -102,7 +102,7 @@ locale_selector () {
 }
 
 keyboard_selector () {
-    input_print "Please insert the keyboard layout to use in console (enter empty to use US, or \"/\" to look up for keyboard layouts): "
+    echo "Please insert the keyboard layout to use in console (enter empty to use US, or \"/\" to look up for keyboard layouts): "
     read -r kblayout
     case "$kblayout" in
         '/') localectl list-keymaps
@@ -112,7 +112,7 @@ keyboard_selector () {
                error_print "The specified keymap doesn't exist."
                return 1
            fi
-        info_print "Changing console layout to $kblayout."
+        echo "Changing console layout to $kblayout."
         loadkeys "$kblayout"
         return 0
     esac
@@ -170,20 +170,20 @@ virt_check
 
 network_installer () {
     case $network_choice in
-        1 ) info_print "Installing and enabling IWD."
+        1 ) echo "Installing and enabling IWD."
             pacstrap /mnt iwd >/dev/null
             systemctl enable iwd --root=/mnt &>/dev/null
             ;;
-        2 ) info_print "Installing and enabling NetworkManager."
+        2 ) echo "Installing and enabling NetworkManager."
             pacstrap /mnt networkmanager >/dev/null
             systemctl enable NetworkManager --root=/mnt &>/dev/null
             ;;
-        3 ) info_print "Installing and enabling wpa_supplicant and dhcpcd."
+        3 ) echo "Installing and enabling wpa_supplicant and dhcpcd."
             pacstrap /mnt wpa_supplicant dhcpcd >/dev/null
             systemctl enable wpa_supplicant --root=/mnt &>/dev/null
             systemctl enable dhcpcd --root=/mnt &>/dev/null
             ;;
-        4 ) info_print "Installing dhcpcd."
+        4 ) echo "Installing dhcpcd."
             pacstrap /mnt dhcpcd >/dev/null
             systemctl enable dhcpcd --root=/mnt &>/dev/null
     esac
@@ -202,7 +202,7 @@ sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
 echo "LANG=$locale" > /mnt/etc/locale.conf
 echo "KEYMAP=$kblayout" > /mnt/etc/vconsole.conf
 
-info_print "Setting hosts file."
+echo "Setting hosts file."
 cat > /mnt/etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
