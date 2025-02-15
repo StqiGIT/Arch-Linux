a8eed4a063411a7153309e8c02f0a3ab5979b89b
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2025-02-15 23:56:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 
+if [ "$(id -u)" -ne 0 ]; then
+        echo "This script must be ran by root" > &2
+        exit 1
+fi
+
 clear
 
 timedatectl set-ntp true
 
 sed -i "s/^#\(Color\)/\1\nILoveCandy/" /etc/pacman.conf
 sed -i "s/^#\(ParallelDownloads\)/\1/" /etc/pacman.conf
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 echo
-echo *------------------------*
+echo *---
 echo *--- Formatting disks ---*
-echo *------------------------*
+echo *---
 echo
 
 read -r -p "Enter installation disk: " system_disk
-
 read -r -p "Enter swap size (in MiB): " swap_size
-swap_calc=$(("$swap_size"+513))
 
-parted "$system_disk" mklabel gpt
-parted "$system_disk" mkpart "EFI" fat32 1MiB 513MiB
-parted "$system_disk" set 1 esp on
-parted "$system_disk" mkpart "SWAP" linux-swap 513MiB "$swap_calc"MiB
-parted "$system_disk" mkpart "ROOT" ext4 "$swap_calc"MiB 100%
+swap_calc=$(("$swap_size"+514))
+root_calc=$(("$swap_calc"+1))
+
+sgdisk -Z ${system_disk}
+sgdisk -a 2048 -o ${system_disk}
+
+sgdisk -n 1:1MiB:513MiB -c 1:"EFI" -t 1:ef00 ${system_disk}
+sgdisk -n 2:514MiB:${swap_calc} -c 2:"SWAP" -t 2:8200 ${system_disk}
+sgdisk -n 3:${root_calc}:0 -c 3:"ROOT" -t 3:8304 ${system_disk}
+
+partprobe "$system_disk"
 
 if [[ "${system_disk}" =~ "/dev/sda" ]] ; then
 	efi_partition="${system_disk}1"
@@ -44,20 +52,20 @@ mkswap "$swap_partition"
 mkfs.ext4 "$root_partition"
 
 echo
-echo *----------------------*
+echo *---
 echo *--- Mounting disks ---*
-echo *----------------------*
+echo *---
 echo
 
-mount "$root_partition" /mnt
 swapon "$swap_partition"
+mount "$root_partition" /mnt
 mkdir /mnt/boot
 mount -o fmask=0137,dmask=0027 "$efi_partition" /mnt/boot
 
 echo
-echo *------------------------------*
+echo *---
 echo *--- Installing base system ---*
-echo *------------------------------*
+echo *---
 echo
 
 read -r -p "Type in your desired kernel (e.g: linux,linux-lts,linux-zen): " kernel
@@ -71,19 +79,23 @@ else
 fi
 
 pacstrap /mnt base base-devel "$kernel" "$kernel"-headers "$microcode" linux-firmware vim
+pacstrap /mnt e2fsprogs dosfstools
+pacstrap /mnt xdg-user-dirs
+pacstrap /mnt git
 
 echo
-echo *------------------------------*
+echo *---
 echo *--- Configuring mirrorlist ---*
-echo *------------------------------*
+echo *---
 echo
 
-curl 'https://archlinux.org/mirrorlist/?country=RU&protocol=https&ip_version=4' | sed -e 's/^#Server/Server/' | rankmirrors -n 5 - > /mnt/etc/pacman.d/mirrorlist
+curl "https://archlinux.org/mirrorlist/?country=RU&protocol=https&ip_version=4" -o /mnt/etc/pacman.d/mirrorlist
+sed -i "s/^#\(Server\)/\1/" /mnt/etc/pacman.d/mirrorlist
 
 echo
-echo *--------------------------*
+echo *---
 echo *--- Configuring pacman ---*
-echo *--------------------------*
+echo *---
 echo
 
 sed -i "s/^#\(Color\)/\1\nILoveCandy/" /mnt/etc/pacman.conf
@@ -91,26 +103,26 @@ sed -i "s/^#\(ParallelDownloads\)/\1/" /mnt/etc/pacman.conf
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
 
 echo
-echo *------------------------*
+echo *---
 echo *--- Generating fstab ---*
-echo *------------------------*
+echo *---
 echo
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo
-echo *----------------------------*
+echo *---
 echo *--- Configuring hostname ---*
-echo *----------------------------*
+echo *---
 echo
 
 read -r -p "Please enter desired host name: " hostname
 echo "$hostname" > /mnt/etc/hostname
 
 echo
-echo *--------------------------*
+echo *---
 echo *--- Configuring locale ---*
-echo *--------------------------*
+echo *---
 echo
 
 read -r -p "Enter locale: " locale
@@ -118,78 +130,87 @@ read -r -p "Enter keyboard layout: " keymap
 
 sed -i "/^#en_US.UTF-8/s/^#//" /mnt/etc/locale.gen
 sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
+
 arch-chroot /mnt locale-gen
-echo "LANG=${locale}.UTF-8" > /mnt/etc/locale.conf
+
+echo "LANG=${locale}" > /mnt/etc/locale.conf
 echo "KEYMAP=${keymap}" > /mnt/etc/vconsole.conf
 
 echo
-echo *------------------------*
-echo *--- Configuring host ---*
-echo *------------------------*
+echo *---
+echo *--- Configuring hosts ---*
+echo *---
 echo
 
-echo -e "127.0.0.1   localhost
+cat > /mnt/etc/hosts <<EOF
+127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   ${hostname}.localdomain   ${hostname}" > /mnt/etc/hosts
+127.0.1.1   ${hostname}.localdomain   ${hostname}
+EOF
 
 echo
-echo *----------------------------*
+echo *---
 echo *--- Configuring timezone ---*
-echo *----------------------------*
+echo *---
 echo
 
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime
 
 echo
-echo *--------------------------------*
+echo *---
 echo *--- Configuring system clock ---*
-echo *--------------------------------*
+echo *---
 echo
 
 arch-chroot /mnt hwclock --systohc
 
 echo
-echo *--------------------------------*
+echo *---
 echo *--- Configuring systemd-boot ---*
-echo *--------------------------------*
+echo *---
 echo
 
 arch-chroot /mnt bootctl --path=/boot install
 
-echo -e "#timeout 10
+cat > /mnt/etc/loader/loader.conf <<EOF
+#timeout 10
 #console-mode max
-default ${kernel}" > /mnt/boot/loader/loader.conf
+default ${kernel}
+EOF
 
 root_UUID=$(blkid -o value -s UUID "${root_partition}")
-echo -e "title Arch Linux (${kernel})
+
+cat > /mnt/boot/loader/entries/"${kernel}".conf <<EOF
+title Arch Linux (${kernel})
 linux /vmlinuz-${kernel}
 initrd /${microcode}.img
 initrd /initramfs-${kernel}.img
-options root=UUID=${root_UUID} rw quiet loglevel=3" > /mnt/boot/loader/entries/"${kernel}".conf
+options root=UUID=${root_UUID} rw quiet loglevel=3
+EOF
 
 echo
-echo *--------------------*
+echo *---
 echo *--- Adding user ---*
-echo *--------------------*
+echo *---
 echo
 
 read -r -p "Enter username: " username
 arch-chroot /mnt useradd -m "$username"
-echo """${username}"" ALL=(ALL:ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/"${username}"
+sed -i "/root ALL=(ALL:ALL) ALL/a${username} ALL=(ALL:ALL) NOPASSWD:ALL" /mnt/etc/sudoers
 
 echo
-echo *--------------------------------------------------*
-echo *--- Setting up "$username" and root passwords ---*
-echo *--------------------------------------------------*
+echo *---
+echo *--- Setting up ${username} password and root password ---*
+echo *---
 echo
 
 arch-chroot /mnt passwd "$username"
 arch-chroot /mnt passwd
 
 echo
-echo *-----------------*
+echo *---
 echo *--- Finishing ---*
-echo *-----------------*
+echo *---
 echo
 
 hypervisor=$(systemd-detect-virt)
@@ -217,41 +238,62 @@ case $hypervisor in
 			;;
 esac
 
-read -r -p "Enter graphics card (e.g: amd,intel,nvidia,none): " graphics_card
-case $graphics_card in
-	amd )		echo "Installing amd drivers."
-			pacstrap /mnt mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
+while true; do
+read -r -p "Enter text editor (e.g: vim,nano,emacs): " editor_selector
+	case $editor_selector in
+		vim )	echo "The text editor vim will be installed"
+ 			pacstrap /mnt vim
+   			break
+     			;;
+		nano )	echo "The text editor nano will be installed"
+       			pacstrap /mnt nano
+	 		break
+   			;;
+		emacs )	echo "The text editor emacs will be installed"
+ 			pacstrap /mnt emacs
+   			break
+     			;;
+       		* )	echo "Enter valid option" > &2
 			;;
-	intel ) 	echo "Installing intel drivers."
-			pacstrap /mnt mesa lib32-mesa vulkan-intel lib32-vulkan-intel
-			;;
-	nvidia )	echo "Installing & Configuring nvidia drivers."
-			pacstrap /mnt nvidia-dkms nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia egl-wayland
-			sed -i '7s/MODULES=(.*)/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /mnt/etc/mkinitcpio.conf
-			echo "options nvidia_drm modeset=1 fbdev=1" > /mnt/etc/modprobe.d/nvidia.conf
-			;;
-	none )		echo "Choosen no graphics card drivers"
-			;;
-	* )		echo "Error: enter valid graphics card name"
-			;;
-esac
+ 	esac
+done
 
-read -r -p "Enter networking utility (e.g: iwd,networkmanager,dhcpd): " network_choice    
-case $network_choice in
-	iwd )	echo "Installing and enabling IWD."
-		pacstrap /mnt iwd
-		systemctl enable iwd --root=/mnt
-		;;
-	networkmanager ) echo "Installing and enabling NetworkManager."
-		pacstrap /mnt networkmanager
-		systemctl enable NetworkManager --root=/mnt
-		;;
-	dhcpd ) echo "Installing dhcpcd."
-		pacstrap /mnt dhcpcd
-		systemctl enable dhcpcd --root=/mnt
-		;;
-	* )	echo "Error: enter valid networking utility name"
-		;;
-esac
+while true; do
+read -r -p "Enter networking utility (e.g: iwd,networkmanager,dhcpd): " network_selector    
+	case $network_selector in
+		iwd )			echo "Installing and enabling IWD."
+					pacstrap /mnt iwd
+					systemctl enable iwd --root=/mnt
+     					break
+					;;
+		networkmanager )	echo "Installing and enabling NetworkManager."
+					pacstrap /mnt networkmanager
+					systemctl enable NetworkManager --root=/mnt
+     					break
+					;;
+		dhcpd ) 		echo "Installing dhcpcd."
+					pacstrap /mnt dhcpcd
+					systemctl enable dhcpcd --root=/mnt
+     					break
+					;;
+		* )			echo "Enter valid option" > &2
+					;;
+	esac
+done
+
+echo
+echo *---
+echo *--- Cleaning up ---*
+echo *---
+echo
+
+arch-chroot /mnt pacman -Scc
+umount -a
+
+echo
+echo *---
+echo *--- Finished, you may reboot now ---*
+echo *---
+echo
 
 exit
