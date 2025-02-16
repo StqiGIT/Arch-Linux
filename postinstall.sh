69d62ba6624dc4159dbec16142dffc630929b2c3
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2025-02-16 21:55:16 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$(id -u)" -eq 0 ]; then
+        echo "This script must NOT be ran by root" >&2
+        exit 1
+fi
+
 read -r -p "Enter graphics card (e.g: amd,intel,nvidia,none): " graphics_card
 case $graphics_card in
 	amd )		echo "Installing amd drivers."
