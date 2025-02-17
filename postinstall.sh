b18f9ce0f1247d794fe9a2ca1284e7f96e2e116f
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2025-02-17 22:15:04 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -21,7 +21,7 @@ rm -rf yay-bin
 
 echo
 echo *---
-echo *--- Installing essentials ---*
+echo *--- Configuring graphics card drivers ---*
 echo *---
 echo
 
@@ -29,16 +29,16 @@ while true; do
 read -r -p "Enter graphics card (e.g: amd,intel,nvidia,none): " graphics_card_selector
 	case $graphics_card_selector in
 		amd )		echo "Installing amd drivers."
-				sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
+				sudo pacman -S --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
     				break
 				;;
 		intel ) 	echo "Installing intel drivers."
-				sudo pacman -S mesa lib32-mesa vulkan-intel lib32-vulkan-intel
+				sudo pacman -S --noconfirm mesa lib32-mesa vulkan-intel lib32-vulkan-intel
     				break
 				;;
 		nvidia )	echo "Installing & Configuring nvidia drivers."
-				pacstrap /mnt nvidia-dkms nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia egl-wayland
-				sed -i '7s/MODULES=(.*)/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /mnt/etc/mkinitcpio.conf
+				sudo pacman -S --noconfirm nvidia-dkms nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia egl-wayland
+				sed -i '7s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /mnt/etc/mkinitcpio.conf
 				echo "options nvidia_drm modeset=1 fbdev=1" > /mnt/etc/modprobe.d/nvidia.conf
     				break
 				;;
@@ -50,17 +50,23 @@ read -r -p "Enter graphics card (e.g: amd,intel,nvidia,none): " graphics_card_se
 	esac
 done
 
+echo
+echo *---
+echo *--- Installing and configuring firewall ---*
+echo *---
+echo
+
 while true; do
 read -r -p "Enter firewall (e.g: nftables,iptables): " firewall_selector
 	case $firewall_selector in
  		nftables )	echo "Instaling & Configuring nftables."
-   				sudo pacman -S nftables iptables-nft
+   				yes | sudo pacman -S --noconfirm nftables iptables-nft
        				echo -e "#!/usr/bin/nft -f\n\nflush ruleset\n\ntable inet filter {\n	chain input {\n		type filter hook input priority 0; policy drop;\n		ct state {established, related} accept\n		iif lo accept\n	}\n	chain forward {\n		type filter hook forward priority 0; policy drop;\n	}\n	chain output {\n		type filter hook output priority 0; policy accept;\n	}\n}" | sudo tee /etc/nftables.conf > /dev/null
 				sudo systemctl enable nftables
 				break
 				;;
 		iptables )	echo "Installing & Configuring iptables."
-  				sudo pacman -S iptables
+  				yes | sudo pacman -S --noconfirm iptables
       				echo -e "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n-A INPUT -s 127.00.1/32 -i lo -j ACCEPT\nCOMMIT" | sudo tee /etc/iptables/iptables.rules > /dev/null
 				sudo systemctl enable iptables
 				break
@@ -70,106 +76,53 @@ read -r -p "Enter firewall (e.g: nftables,iptables): " firewall_selector
 	esac
 done
 
-#
-# Checking variables
-#
-
-username=$(whoami)
+echo
+echo *---
+echo *--- Installing desktop ---*
+echo *---
+echo
 
 while true; do
-read -r -p "Enter Enter networking utility (e.g: iwd,networkmanager,dhcpd): " network_selector
-	case $network_selector in
-		iwd )			net_gui=iwgtk
-     					break
-					;;
-		networkmanager )	net_gui=nm-connection-editor
-     					break
-					;;
-		dhcpd )			break
-					;;
-		* )			echo "Enter valid option" >&2
-					;;
+read -r -p "Enter desktop (e.g: kde,gnome,hyprland): " desktop_selector
+	case $desktop_selector in
+		kde )		echo "Installing KDE Plasma"
+				yay -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-ms-fonts
+				yay -S --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack
+				yay -S --noconfirm gstreamer gst-libav gst-plugin-pipewire gst-plugins-ugly gst-plugins-bad gst-plugins-base gst-plugins-good
+    				yay -S --noconfirm qt5-wayland qt5-graphicaleffects qt5-multimedia qt5-quickcontrols qt5-quickcontrols2 qt6-wayland qt6-multimedia
+				yay -S --noconfirm flatpak
+    				yay -S --noconfirm plasma-meta
+				yay -S --noconfirm dolphin dolphin-plugins gwenview kcalc kate konsole ktorrent
+				sudo systemctl enable sddm
+				break
+				;;
+		gnome )		echo "Installing GNOME"
+  				yay -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-ms-fonts
+				yay -S --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack
+				yay -S --noconfirm gstreamer gst-libav gst-plugin-pipewire gst-plugins-ugly gst-plugins-bad gst-plugins-base gst-plugins-good
+				yay -S --noconfirm flatpak
+    				yay -S --noconfirm gnome
+    				yay -S --noconfirm transmission-gtk
+				sudo systemctl enable gdm
+				break
+				;;
+		hyprland )	echo "Installing Hyprland"
+  				yay -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd ttf-ms-fonts
+				yay -S --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack
+				yay -S --noconfirm gstreamer gst-libav gst-plugin-pipewire gst-plugins-ugly gst-plugins-bad gst-plugins-base gst-plugins-good
+        			yay -S --noconfirm qt5-wayland qt6-wayland
+				yay -S --noconfirm flatpak
+				yay -S --noconfirm hyprland hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+        			yay -S --noconfirm grim slurp
+    				yay -S --noconfirm blueman nm-connection-editor pavucontrol
+				yay -S --noconfirm gvfs gvfs-mtp thunar thunar-volman tumbler mousepad ristretto
+    				yay -S --noconfirm transmission-gtk
+				break
+				;;
 	esac
 done
 
-#
-# Checking variables end
-#
-
-echo
-echo *---
-echo *--- Installing fonts ---*
-echo *---
-echo
-
-yay -S noto-fonts noto-fonts-cjk noto-fonts-emoji
-yay -S ttf-jetbrains-mono-nerd
-yay -S ttf-ms-fonts
-
-echo
-echo *---
-echo *--- Installing GUI essentials ---*
-echo *---
-echo
-
-yay -S pipewire pipewire-pulse pipewire-alsa pipewire-jack
-yay -S gstreamer gst-libav gst-plugin-pipewire gst-plugins-ugly gst-plugins-bad gst-plugins-base gst-plugins-good
-yay -S gvfs gvfs-mtp
-yay -S bluez openrgb
-yay -S flatpak transmission-gtk
-yay -S qt5-wayland qt6-wayland
-
-echo
-echo *---
-echo *--- Installing hyprland ---*
-echo *---
-echo
-
-yay -S hyprland xdg-desktop-portal-hyprland
-yay -S hyprpaper hyprpolkitagent-git
-
-echo
-echo *---
-echo *--- Installing GUI apps ---*
-echo *---
-echo
-
-yay -S blueman pavucontrol ${network_gui}
-yay -S qt5ct qt6ct nwg-look
-yay -S grim slurp
-yay -S thunar thunar-volman tumbler
-yay -S mousepad
-yay -S libreoffice-fresh-ru
-yay -S ristretto gimp mpv
-yay -S foot waybar wofi wlogout mako
-yay -S firefox
-yay -S steam vesktop-bin
-
-echo
-echo *---
-echo *--- Configuring installed apps ---*
-echo *---
-echo
-
-sudo systemctl enable bluetooth
-sudo modprobe i2c-dev i2c-piix4
-
-while true; do
-read -r -p "Do you want to copy preconfigured setup?: (e.g: yes,no): " copy_selector
-	case $copy_selector in
-		yes )	read -r -p "Enter external partition with config files: " external_partition
-  			sudo mount ${external_partition} /mnt
-			cp -r /mnt/Arch-Linux/dotfiles/{.config/,.local/,.scripts/} ~/
-			umount ${external_partition}
-			break
-			;;
-		no )	echo "Preconfigured setup will not be installed"
-			break
-			;;
-		* )	echo "Enter valid option" >&2
-  			;;
-     esac
-done
+yay -S --noconfirm openrgb libreoffice-fresh-ru gimp mpv firefox vesktop-bin steam
 
 echo
 echo *---
@@ -183,8 +136,7 @@ sudo pacman -Scc
 
 echo
 echo *---
-echo *--- Finished, you may reboot now ---*
-echo *--- Run hyprland via 'sh .scripts/hyprland.sh' ---*
+echo *--- Finished, it is advised reboot now ---*
 echo *---
 echo
 
