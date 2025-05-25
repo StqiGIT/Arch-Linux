b7ed2262c68557ce0022a0313df94ed7dd0e8e20
StqiGIT <147992799+StqiGIT@users.noreply.github.com>
2025-05-25 04:25:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,48 +18,87 @@ sed -i "s/^#\(Color\)/\1\nILoveCandy/" /etc/pacman.conf
 sed -i "s/^#\(ParallelDownloads\)/\1/" /etc/pacman.conf
 reflector --country Russia --delay 24 --score 10 --save /etc/pacman.d/mirrorlist
 
+clear
+
 echo
 echo *---
 echo *--- Formatting disks ---*
 echo *---
 echo
 
-lsblk -o NAME,FSTYPE,SIZE
-echo
-read -r -p "Enter installation disk: " system_disk
-echo
-read -r -p "Enter swap size (in MiB): " swap_size
+while true; do
+	echo
+	lsblk -o NAME,FSTYPE,SIZE
+	echo
+	read -r -p "Enter installation disk: " installation_disk_selector
+        	if [ ! -b "$installation_disk_selector" ]; then
+                	echo "Error: $installation_disk_selector is not a viable block device" >&2
+                	exit 1
+                	continue
+        	fi
+       		break
+done
 
-swap_calc=$(("$swap_size"+514))
-root_calc=$(("$swap_calc"+1))
+sgdisk -Z /dev/"$installation_disk_selector"
+sgdisk -a 2048 -o /dev/"$installation_disk_selector"
 
-sgdisk -Z /dev/"$system_disk"
-sgdisk -a 2048 -o /dev/"$system_disk"
+sgdisk -n 1:1MiB:513MiB -c 1:"EFI" -t 1:ef00 /dev/"$installation_disk_selector"
+sgdisk -n 2:514MiB:${swap_calc}MiB -c 2:"SWAP" -t 2:8200 /dev/"$installation_disk_selector"
+sgdisk -n 3:${root_calc}MiB:0 -c 3:"ROOT" -t 3:8304 /dev/"$installation_disk_selector"
 
-sgdisk -n 1:1MiB:513MiB -c 1:"EFI" -t 1:ef00 /dev/"$system_disk"
-sgdisk -n 2:514MiB:${swap_calc}MiB -c 2:"SWAP" -t 2:8200 /dev/"$system_disk"
-sgdisk -n 3:${root_calc}MiB:0 -c 3:"ROOT" -t 3:8304 /dev/"$system_disk"
+partprobe /dev/"$installation_disk_selector"
 
-partprobe /dev/"$system_disk"
+if [[ "/dev/${installation_disk_selector}" =~ "/dev/sd" ]] ; then
+	efi_partition="/dev/${installation_disk_selector}1"
+	swap_partition="/dev/${installation_disk_selector}2"
+	root_partition="/dev/${installation_disk_selector}3"	
 
-if [[ "/dev/${system_disk}" =~ "/dev/sd" ]] ; then
-	efi_partition="/dev/${system_disk}1"
-	swap_partition="/dev/${system_disk}2"
-	root_partition="/dev/${system_disk}3"
-	
-elif [[ "${system_disk}" =~ "/dev/vd" ]] ; then
-	efi_partition="/dev/${system_disk}1"
-	swap_partition="/dev/${system_disk}2"
-	root_partition="/dev/${system_disk}3"
+elif [[ "/dev/${installation_disk_selector}" =~ "/dev/vd" ]] ; then
+	efi_partition="/dev/${installation_disk_selector}1"
+	swap_partition="/dev/${installation_disk_selector}2"
+	root_partition="/dev/${installation_disk_selector}3"
 else
-	efi_partition="/dev/${system_disk}p1"
-	swap_partition="/dev/${system_disk}p2"
-	root_partition="/dev/${system_disk}p3"
+	efi_partition="/dev/${installation_disk_selector}p1"
+	swap_partition="/dev/${installation_disk_selector}p2"
+	root_partition="/dev/${installation_disk_selector}p3"
 fi
 
-mkfs.fat -F 32 "$efi_partition"
-mkswap "$swap_partition"
-mkfs.ext4 "$root_partition"
+while true; do
+	echo
+	echo "Filesystem options: "
+	echo
+	echo "ext4"
+	echo "----------"
+	echo "xfs"
+	echo
+	read -r -p "Enter root filesystem: " root_filesystem_selector
+		case $root_filesystem_selector in
+			ext4 )	echo
+				mkfs.fat -F 32 "$efi_partition"
+				mkswap "$swap_partition"
+				mkfs.ext4 "$root_partition"
+
+				root_filesystem_progs="e2fsprogs"
+
+				break
+				;;
+			xfs )	echo
+				mkfs.fat -F 32 "$efi_partition"
+				mkswap "$swap_partition"
+				mkfs.xfs "$root_partition"
+
+				root_filesystem_progs="xfsprogs"
+
+				break
+				;;
+			* )	echo
+				echo "Enter valid option" >&2
+				echo
+				;;
+		esac
+done
+
+clear
 
 echo
 echo *---
@@ -78,33 +117,6 @@ echo *--- Installing base system ---*
 echo *---
 echo
 
-while true; do
-	echo
- 	echo "Kernel options:"
-  	echo
-  	echo "linux"
-  	echo "----------"
-  	echo "linux-lts"
-  	echo "----------"
-  	echo "linux-zen"
-  	echo
-	read -r -p "Enter kernel: " kernel
-	case $kernel in
-		linux )		kernel="linux"
-  				break
-      				;;
-		linux-lts )	kernel="linux-lts"
-  				break
-      				;;
-		linux-zen )	kernel="linux-zen"
-  				break
-      				;;
-		* )		echo "Enter valid option" >&2
-  				;;
-      esac
-done
-  
-
 if [[ "$(grep vendor_id /proc/cpuinfo)" == *"AuthenticAMD"* ]]; then
 	echo
 	echo "An AMD CPU has been detected, the AMD microcode will be installed."
@@ -117,15 +129,46 @@ else
 	microcode="intel-ucode"
 fi
 
-pacstrap /mnt base base-devel "$kernel" "$kernel"-headers "$microcode" linux-firmware
+pacstrap /mnt base base-devel linux linux-headers "$microcode" linux-firmware
+pacstrap /mnt "$root_filesystem_progs" dosfstools exfat-utils
+pacstrap /mnt reflector git wget curl
 pacstrap /mnt p7zip zip unzip
-pacstrap /mnt e2fsprogs dosfstools exfat-utils
-pacstrap /mnt xdg-user-dirs reflector
-pacstrap /mnt git curl
-pacstrap /mnt networkmanager bluez
+pacstrap /mnt vim
 
-systemctl enable NetworkManager --root=/mnt
-systemctl enable bluetooth --root=/mnt
+hypervisor=$(systemd-detect-virt)
+case $hypervisor in
+	kvm )		echo
+ 			echo "KVM has been detected, setting up guest tools."
+    			echo
+			pacstrap /mnt qemu-guest-agent
+			systemctl enable qemu-guest-agent --root=/mnt
+			;;
+	vmware )	echo
+ 			echo "VMWare Workstation/ESXi has been detected, setting up guest tools."
+    			echo
+			pacstrap /mnt open-vm-tools
+			systemctl enable vmtoolsd --root=/mnt
+			systemctl enable vmware-vmblock-fuse --root=/mnt
+			;;
+	oracle )	echo
+ 			echo "VirtualBox has been detected, setting up guest tools."
+ 			echo
+			pacstrap /mnt virtualbox-guest-utils
+			systemctl enable vboxservice --root=/mnt
+			;;
+	microsoft )	echo
+ 			echo "Hyper-V has been detected, setting up guest tools."
+ 			echo
+			pacstrap /mnt hyperv
+			systemctl enable hv_fcopy_daemon --root=/mnt
+			systemctl enable hv_kvp_daemon --root=/mnt
+			systemctl enable hv_vss_daemon --root=/mnt
+			;;
+	* )		echo
+ 			echo "No Hypervisor has been detected"
+ 			echo
+			;;
+esac
 
 echo
 echo *---
@@ -157,9 +200,15 @@ echo *--- Configuring hostname ---*
 echo *---
 echo
 
-read -r -p "Enter hostname: " hostname
-
-echo "$hostname" > /mnt/etc/hostname
+while true; do
+	read -r -p "Enter hostname: " hostname_selector
+		if [ -z "$hostname_selector" ]; then
+			echo "Error: username cannot be empty, enter valid option"
+			continue
+		fi
+			echo "$hostname_selector" > /mnt/etc/hostname
+			break
+done
 
 echo
 echo *---
@@ -169,7 +218,7 @@ echo
 
 cat > /mnt/etc/hosts <<EOF
 127.0.0.1   localhost.localdomain localhost
-127.0.1.1   ${hostname}.lan   ${hostname}
+127.0.1.1   ${hostname_selector}.lan   ${hostname_selector}
 
 ::1         ip6-localhost	ip6-loopback
 EOF
@@ -180,25 +229,34 @@ echo *--- Configuring locale ---*
 echo *---
 echo
 
-read -r -p "Enter locale: " locale
+read -r -p "Enter locale: " locale_selector
 
-if [ "$locale" = "" ]; then
+if [ "$locale_selector" = "" ]; then
 	sed -i "/^#en_US.UTF-8/s/^#//" /mnt/etc/locale.gen
 else
 	sed -i "/^#en_US.UTF-8/s/^#//" /mnt/etc/locale.gen
-	sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
+	sed -i "/^#$locale_selector/s/^#//" /mnt/etc/locale.gen
 fi
 
 arch-chroot /mnt locale-gen
-echo "LANG=${locale}" > /mnt/etc/locale.conf
+echo "LANG=${locale_selector}" > /mnt/etc/locale.conf
 
-if [ "$locale" = "" ]; then
-	echo FONT=cyr-sun16 > /mnt/etc/vconsole.conf
-else
-	echo
-	read -r -p "Enter keymap: " keymap
-	echo -e "KEYMAP=${keymap}\nFONT=cyr-sun16" > /mnt/etc/vconsole.conf
-fi
+while true; do
+	if [ "$locale_selector" = "" ]; then
+		echo FONT=cyr-sun16 > /mnt/etc/vconsole.conf
+		break
+	else
+		echo
+		read -r -p "Enter keymap: " keymap_selector
+			if [ -z "$keymap_selector" ]; then
+				echo "Error: keymap cannot be empty, enter valid option"
+				continue
+			fi
+				echo -e "KEYMAP=${keymap_selector}\nFONT=cyr-sun16" > /mnt/etc/vconsole.conf
+				break
+	fi
+		continue
+done
 
 echo
 echo *---
@@ -206,18 +264,68 @@ echo *--- Configuring users ---*
 echo *---
 echo
 
-read -r -p "Enter username: " username
+while true; do
+	read -r -p "Add user?: " additional_user_selector
+		case $additional_user_selector in
+			yes )	echo
+				read -r -p "Enter username: " additional_user_username
 
-arch-chroot /mnt useradd -m "$username"
+				if [ -z "$additional_user_username" ]; then
+					echo "Error: username cannot be empty, enter valid option"
+					continue
+				fi
 
-echo
-echo "Enter ${username} password"
-arch-chroot /mnt passwd "$username"
-echo
-echo "Enter root password"
-arch-chroot /mnt passwd
+				arch-chroot /mnt useradd -m "$additional_user_username"
+				break
+				;;
+			no )	echo
+				break
+				;;
+			* )	echo
+				echo "Enter valid option" >&2
+				;;
+		esac
+done
 
-sed -i "/root ALL=(ALL:ALL) ALL/a${username} ALL=(ALL:ALL) NOPASSWD:ALL" /mnt/etc/sudoers
+if id "$additional_user_username" $>/dev/null; then
+	while true; do
+		echo
+		echo "Enter ${$additional_user_username} password"
+		arch-chroot /mnt passwd "$additional_user_username"
+		if [ $? -eq 0 ]; then
+			echo "Password changed successfully"
+			break
+		elif
+			echo "Error: password change failed, enter valid option"
+			sleep 1
+		fi
+
+		echo "{$additional_user_username} ALL=(ALL:ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/{$additional_user_username}
+
+		echo
+		echo "Enter root password"
+		arch-chroot /mnt passwd root
+		if [ $? -eq 0 ]; then
+			echo "Password changed successfully"
+			break
+		elif
+			echo "Error: password change failed, enter valid option"
+			sleep 1
+		fi
+	done
+elif
+	while true; do
+		echo
+		echo "Enter root password"
+		arch-chroot /mnt passwd root
+		if [ $? -eq 0 ]; then
+			echo "Password changed successfully"
+			break
+		elif
+			echo "Error: password change failed, enter valid option"
+			sleep 1
+		fi
+fi
 
 echo
 echo *---
@@ -251,15 +359,15 @@ arch-chroot /mnt bootctl --path=/boot install
 cat > /mnt/boot/loader/loader.conf <<EOF
 #timeout 10
 #console-mode max
-default ${kernel}.conf
+default linux.conf
 EOF
 
-cat > /mnt/boot/loader/entries/"${kernel}".conf <<EOF
-title Arch Linux (${kernel})
-linux /vmlinuz-${kernel}
+cat > /mnt/boot/loader/entries/linux.conf <<EOF
+title Arch Linux (linux)
+linux /vmlinuz-linux
 initrd /${microcode}.img
-initrd /initramfs-${kernel}.img
-options root=UUID=$(blkid -o value -s UUID "$root_partition") rw quiet loglevel=3 i8042.nopnp
+initrd /initramfs-linux.img
+options root=UUID=$(blkid -o value -s UUID "$root_partition") rw quiet
 EOF
 
 echo
@@ -267,76 +375,6 @@ echo *---
 echo *--- Finishing ---*
 echo *---
 echo
-
-hypervisor=$(systemd-detect-virt)
-case $hypervisor in
-	kvm )		echo
- 			echo "KVM has been detected, setting up guest tools."
-    			echo
-			pacstrap /mnt qemu-guest-agent
-			systemctl enable qemu-guest-agent --root=/mnt
-			;;
-	vmware )	echo
- 			echo "VMWare Workstation/ESXi has been detected, setting up guest tools."
-    			echo
-			pacstrap /mnt open-vm-tools
-			systemctl enable vmtoolsd --root=/mnt
-			systemctl enable vmware-vmblock-fuse --root=/mnt
-			;;
-	oracle )	echo
- 			echo "VirtualBox has been detected, setting up guest tools."
- 			echo
-			pacstrap /mnt virtualbox-guest-utils
-			systemctl enable vboxservice --root=/mnt
-			;;
-	microsoft )	echo
- 			echo "Hyper-V has been detected, setting up guest tools."
- 			echo
-			pacstrap /mnt hyperv
-			systemctl enable hv_fcopy_daemon --root=/mnt
-			systemctl enable hv_kvp_daemon --root=/mnt
-			systemctl enable hv_vss_daemon --root=/mnt
-			;;
-	* )		echo
- 			echo "Error: unknown hypervisor"
- 			echo
-			;;
-esac
-
-while true; do
-	echo
- 	echo "Editor options:"
-  	echo
-  	echo "vim"
-  	echo "----------"
-  	echo "nano"
-  	echo "----------"
-  	echo "emacs"
-  	echo
-	read -r -p "Enter text editor: " editor_selector
-	case $editor_selector in
-		vim )	echo
-  			echo "The text editor vim will be installed"
-     			echo
- 			pacstrap /mnt vim
-   			break
-     			;;
-		nano )	echo
-  			echo "The text editor nano will be installed"
-     			echo
-       			pacstrap /mnt nano
-	 		break
-   			;;
-		emacs )	echo
-  			echo "The text editor emacs will be installed"
-     			echo
- 			pacstrap /mnt emacs
-   			break
-     			;;
-       		* )	echo "Enter valid option" >&2
-			;;
- 	esac
-done
 
 echo
 echo *---
